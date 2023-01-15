af2ed3895e66ae65b3ccad0c3f530f517e1c0c13
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2023-01-15 18:46:49 +0100

    m1

---
+++ src/app/professori/professori.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfessoriRoutingModule } from './professori-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProfessoriRoutingModule
+  ]
+})
+export class ProfessoriModule { }
