69feb9ab07d84c164a97152cc4ace00240ef924a
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2023-01-15 18:11:32 +0100

    add index comp

--- src/app/studenti/studenti.module.ts
+++ src/app/studenti/studenti.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentiRoutingModule } from './studenti-routing.module';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
     CommonModule,
     StudentiRoutingModule
