90940fa991891854b8364ceaa2315410a21ca53a
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2023-01-15 17:55:54 +0100

    add studenti module

---
+++ src/app/studenti/studenti.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StudentiRoutingModule } from './studenti-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StudentiRoutingModule
+  ]
+})
+export class StudentiModule { }
